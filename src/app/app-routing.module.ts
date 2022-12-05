d11ddc29f147e8cd5a7b63ac1b05929f79840473
bangkitwira <bangkitwira@gmail.com>
2022-12-05 20:45:13 +0700

    initialize project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { IdeasComponent } from './views/ideas/ideas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'ideas',
+    component: IdeasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { LazyImageLoadingDirective } from './shared/directives/image.directive';
+import { BannerComponent } from './shared/components/banner/banner.component';
+import { IdeasComponent } from './views/ideas/ideas.component';
+import { DropdownComponent } from './shared/components/dropdown/dropdown.component';
+import { HomeComponent } from './views/home/home.component';
+import { CardPostComponent } from './shared/components/card-post/card-post.component';
+import { PaginationComponent } from './shared/components/pagination/pagination.component';
+import { CardPlaceholderComponent } from './shared/components/card-placeholder/card-placeholder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LazyImageLoadingDirective,
+    BannerComponent,
+    IdeasComponent,
+    DropdownComponent,
+    HomeComponent,
+    CardPostComponent,
+    PaginationComponent,
+    CardPlaceholderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
